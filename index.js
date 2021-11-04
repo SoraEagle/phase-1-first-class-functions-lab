d0318426c08813c9bd93370d97478224037845d7
Matthew <phillips.matthew829@gmail.com>
2021-11-04 11:54:38 -0500

    commit tree 6c15ca1f893a8528c89110d8e5d152734a89fbce

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function (){
+    const copyDrivers = drivers.slice();
+    copyDrivers.pop();
+    copyDrivers.pop();
+    return copyDrivers;
+};
+
+const returnLastTwoDrivers = function (){
+    const copyDrivers = drivers.slice();
+    copyDrivers.shift();
+    copyDrivers.shift();
+    return copyDrivers;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue){
+    return function (value){
+        return multiplyValue * value;
+    }; //end of callback/nested function
+}; //end out outer function
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, chooseDrivers){
+    return chooseDrivers();
+}
